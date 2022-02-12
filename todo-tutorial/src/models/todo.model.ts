2859868d0aa6f5b4f96d9dd73c749624a401a80d
sudish kumar <sudishkumar.edugaon@gmail.com>
2022-02-12 09:28:45 +0530

    todo list is added

--- todo-tutorial/src/models/todo.model.ts
+++ todo-tutorial/src/models/todo.model.ts
@@ -1,4 +1,6 @@
-import {Entity, model, property} from '@loopback/repository';
+import {Entity, model, property, belongsTo} from '@loopback/repository';
+import {TodoList, TodoListWithRelations} from './todo-list.model';
+
 
 @model()
 export class Todo extends Entity {
@@ -26,6 +28,8 @@ export class Todo extends Entity {
   })
   desc: string;
 
+  @belongsTo(() => TodoList)
+  todoListId: number;
 
   constructor(data?: Partial<Todo>) {
     super(data);
@@ -34,6 +38,8 @@ export class Todo extends Entity {
 
 export interface TodoRelations {
   // describe navigational properties here
+  todoList?: TodoListWithRelations;
+
 }
 
 export type TodoWithRelations = Todo & TodoRelations;
